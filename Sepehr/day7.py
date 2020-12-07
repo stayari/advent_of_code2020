3c629e49f4660957723a9bc36fc21fd78671a2a9
Sepehr Tayari <sepehrtayari@gmail.com>
2020-12-07 22:28:32 +0100

    Day7 part2 done. But code needs to be cleaned up

--- Sepehr/day7.py
+++ Sepehr/day7.py
@@ -2,35 +2,41 @@ class Day7:
 
     def __init__(self):
         self.bag_dict = dict()
-        self.holder_bag = dict()
-
+        self.bag_containing_spec_color = dict()
         self._format_data()
+        self.color_to_find = 'shinygoldbags'
+        self.total_bags_in_gold()
 
+
+    def total_bags_in_gold(self):
+        res = self._total_bags_in_gold('shinygoldbags')
+        print(res)
+        return res
+
+    def _total_bags_in_gold(self, bag_color):
+        total_bags = 0
+        for bag in self.bag_dict[bag_color]:
+            if bag == 'nootherbags':
+                print ("returning from rec on color.{}".format(bag_color)) 
+                return 0
+            total_bags = total_bags + int(self.bag_dict[bag_color][bag]) * (self._total_bags_in_gold(bag) +1)
+        return total_bags
 
     def _find_gold(self, bag_color):
         depth_counter = 1
         for bag in self.bag_dict:
-            self.holder_bag[bag_color] ='New color!'
+            self.bag_containing_spec_color[bag_color] ='New color!'
             if bag_color in self.bag_dict[bag]:
                 self._find_gold(bag)
                 #depth_counter += self._find_gold(bag)
         return depth_counter
 
-
     def find_gold(self):
         self._find_gold('shinygoldbags')
         # Ta bort raden shinygoldbag för vi får fel med ett
-        self.holder_bag.pop('shinygoldbags')
-        return len(self.holder_bag)
-
-    def find_gold_gammal(self):
-        for bag in self.bag_dict:
-            if 'shinygoldbags' in self.bag_dict[bag]:
-                self._find_gold(bag)
-                #gold_counter += self._find_gold(bag)
-        return len(self.holder_bag)
-        #print(gold_counter)
-
+        self.bag_containing_spec_color.pop('shinygoldbags')
+        print(self.bag_containing_spec_color)
+        return len(self.bag_containing_spec_color)
 
     def _format_data(self):
         input = open('inputs/input7.txt', 'r')
@@ -38,9 +44,8 @@ class Day7:
             # Shitty code w/e
             line = line.replace(' ', '').replace('.', '').strip()
             bag_info = line.split('contain')
-            if bag_info[1] == 'nootherbags':
-                print('tom väska')
-            else: 
+            # If line contains 'nootherbags' there will be no ','
+            if bag_info[1] != 'nootherbags':
                 bag_info[1:] = bag_info[1].split(',')
             self._add_bag_info(bag_info)
         self.find_gold()
@@ -63,4 +68,4 @@ class Day7:
 if __name__ == '__main__':
     puzzle = Day7()
     #puzzle.print()
-    print(len(puzzle.holder_bag))
+    print(len(puzzle.bag_containing_spec_color))
